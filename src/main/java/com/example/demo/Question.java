ba3885975bd7274996908d882ac3fa5e368575f2
youngguen lee <[200~guenguen9797@gmail.comgit config --global user.emailbacke]|git config --global user.email [200~guenguen9797@gmail.com>
2023-09-27 17:04:09 +0900

    콜럼 추가

--- src/main/java/com/example/demo/Question.java
+++ src/main/java/com/example/demo/Question.java
@@ -1,12 +1,11 @@
 package com.example.demo;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -15,5 +14,15 @@ public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(length = 1000)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+
 
 }
