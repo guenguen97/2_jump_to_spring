c71a0cb5a3a271c79da279a10ac7a07518d7bc6f
guenguen97 <guenguen97@gmail.com>
2023-09-29 18:16:12 +0900

    검색 기능 까지 추가 끝

--- src/main/java/com/example/demo/Question/QuestionRepository.java
+++ src/main/java/com/example/demo/Question/QuestionRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.Question;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
@@ -12,5 +13,7 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubjectAndContent(String subject , String content);
     List<Question> findBySubjectLike(String subject);
     Page<Question> findAll(Pageable pageable);
+
+    Page<Question> findAll(Specification<Question> spec, Pageable pageable);
 }
 
