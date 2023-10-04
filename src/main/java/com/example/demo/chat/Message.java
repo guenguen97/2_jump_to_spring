a8e5d1451d32780de4544eea50be6a4cdd781b2c
youngguen lee <[200~guenguen9797@gmail.comgit config --global user.emailbacke]|git config --global user.email [200~guenguen9797@gmail.com>
2023-10-04 23:57:14 +0900

    채팅 시도

---
+++ src/main/java/com/example/demo/chat/Message.java
@@ -0,0 +1,13 @@
+package com.example.demo.chat;
+
+
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Message {
+    private String content;
+    private String sender;
+}
