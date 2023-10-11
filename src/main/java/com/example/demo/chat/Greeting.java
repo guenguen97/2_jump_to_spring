5d3b82d13eea9947807957f8506d412b40338380
youngguen lee <[200~guenguen9797@gmail.comgit config --global user.emailbacke]|git config --global user.email [200~guenguen9797@gmail.com>
2023-10-11 23:07:55 +0900

    채팅기록 디비에 저장

--- src/main/java/com/example/demo/chat/Greeting.java
+++ src/main/java/com/example/demo/chat/Greeting.java
@@ -1,13 +1,22 @@
 package com.example.demo.chat;
 
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Greeting {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
     private String content;
+
+
 
     public Greeting(String content) {
         this.content = content;
--- src/main/java/com/example/demo/chat/GreetingController.java
+++ src/main/java/com/example/demo/chat/GreetingController.java
@@ -1,20 +1,28 @@
 package com.example.demo.chat;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
+
+@RequiredArgsConstructor
 @Controller
 public class GreetingController {
 
+    private final GreetingRepository greetingRepository;
 
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
-        Thread.sleep(1000); // simulated delay
-        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+        Thread.sleep(10); // simulated delay
+        Greeting greeting= new Greeting(message.getName());
+        greetingRepository.save(greeting);
+
+
+        return new Greeting( HtmlUtils.htmlEscape(message.getName()) );
     }
 
 }
